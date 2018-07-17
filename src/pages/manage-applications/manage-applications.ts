07f409b4433c0e389ddfc291a005ab4d70aeca93
Vishal Gandhi <vkgandhi84@gmail.com>
2018-07-16 21:12:33 -0400

    Updated profile page along with manage application page.

--- src/pages/manage-applications/manage-applications.ts
+++ src/pages/manage-applications/manage-applications.ts
@@ -25,6 +25,7 @@ export class ManageApplicationsPage {
   searchTerm: string = '';
   searchTimer: any;
   items: any;
+  searcheditems: any;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
@@ -32,10 +33,12 @@ export class ManageApplicationsPage {
     private shareProvider: ShareProvider,
     public loadingCtrl: LoadingController
   ) {
-    this.data.pendingapptitle = "Pending Applications:"
-    this.items = [
-      //[1, 'Vishal Gandhi','07-01-2018','Pending'], [2, 'Rafael Alix','07-10-2018','Review'], [3, 'Newark President','07-15-2018','Accepted'], [1, 'Vishal Gandhi','07-01-2018','Pending'], [2, 'Rafael Alix','07-10-2018','Review'], [3, 'Newark President','07-15-2018','Accepted'], [1, 'Vishal Gandhi','07-01-2018','Pending'], [2, 'Rafael Alix','07-10-2018','Review'], [3, 'Newark President','07-15-2018','Accepted'], [1, 'Vishal Gandhi','07-01-2018','Pending'], [2, 'Rafael Alix','07-10-2018','Review'], [3, 'Newark President','07-15-2018','Accepted'], [1, 'Vishal Gandhi','07-01-2018','Pending'], [2, 'Rafael Alix','07-10-2018','Review'], [3, 'Newark President','07-15-2018','Accepted'], [1, 'Vishal Gandhi','07-01-2018','Pending'], [2, 'Rafael Alix','07-10-2018','Review'], [3, 'Newark President','07-15-2018','Accepted'], [1, 'Vishal Gandhi','07-01-2018','Pending'], [2, 'Rafael Alix','07-10-2018','Review'], [3, 'Newark President','07-15-2018','Accepted']
-    ];
+    this.data.pendingappflag = true;
+    this.data.pendingapptitle = "Pending Applications";
+    this.data.searchappflag = false;
+    this.data.searchapptitle = "";
+    this.items = [];
+    this.searcheditems = [];
   }
 
   ionViewWillLoad() {
@@ -90,19 +93,89 @@ export class ManageApplicationsPage {
             }
             else {
               this.data.error = "Unknown problem occured.  Please contact administrator.";
+              this.shareProvider.presentMessageOnlyAlert("Unknown problem occured.  Please contact administrator.");
+              console.log("Unknown problem occured.  Please contact administrator.  Code: Manage-Applications-001");
               this.loading.dismissAll();
             }
           }
         },
         error => {
           this.data.error = "Unknown problem occured.  Please contact administrator.";
+          this.shareProvider.presentMessageOnlyAlert("Unknown problem occured.  Please contact administrator.");
+          console.log("Unknown problem occured.  Please contact administrator.  Code: Manage-Applications-002");
           this.loading.dismissAll();
         }
       );
   }
 
   searchApplications() {
-    console.log(this.searchTerm);
+    //console.log(this.searchTerm);
+    if(this.searchTerm == '') {
+      this.data.searchappflag = false;
+      this.data.pendingappflag = true;
+    }
+    else {
+      //===============================
+      this.loading = this.loadingCtrl.create({
+        content: '',
+      });
+      this.loading.present();
+  
+      var decoded_response = "";
+      var body = new FormData();
+      body.append('sessionid', this.shareProvider.sessionid);
+      body.append('search_text', this.searchTerm);
+      this.http
+        .post(this.shareProvider.server + "application/searchapplications.php", body)
+        .subscribe(
+          data => {
+            //console.log(data["_body"]);
+            var mydt, mm, dd, yyyy = null;
+            //console.log(data["_body"]);
+            decoded_response = JSON.parse(data["_body"]);
+            if (decoded_response[0] == "true") {
+              for(var i=0; i<decoded_response[2].length; i++) {
+                decoded_response[2][i]["dttmcreated"] = decoded_response[2][i]["dttmcreated"].substring(5,7) + '-' + decoded_response[2][i]["dttmcreated"].substring(8,10) + '-' + decoded_response[2][i]["dttmcreated"].substring(0,4);
+              }
+              this.searcheditems = decoded_response[2];
+              if(this.searcheditems.length > 0) {
+                this.data.searchapptitle = "Applications for search term: "+this.searchTerm;
+              }
+              else {
+                this.data.searchapptitle = "NO Applications Found for search term: "+this.searchTerm;
+              }
+              this.data.searchappflag = true;
+              this.data.pendingappflag = false;
+              this.loading.dismissAll();
+            }
+            else if(decoded_response[0] == "false") {
+              this.data.searchapptitle = decoded_response[1];
+              this.data.searchappflag = true;
+              this.data.pendingappflag = false;
+              this.loading.dismissAll();
+            }
+            else {
+              if((decoded_response[1] == 'Session Expired.') || (decoded_response[1] == 'Invalid Session.')) {
+                this.navCtrl.push("LoginPage", { data: 'Please login again.' });
+                this.loading.dismissAll();
+              }
+              else {
+                this.data.error = "Unknown problem occured.  Please contact administrator.";
+                this.shareProvider.presentMessageOnlyAlert("Unknown problem occured.  Please contact administrator.");
+                console.log("Unknown problem occured.  Please contact administrator.  Code: Manage-Applications-003");
+                this.loading.dismissAll();
+              }
+            }
+          },
+          error => {
+            this.data.error = "Unknown problem occured.  Please contact administrator.";
+            this.shareProvider.presentMessageOnlyAlert("Unknown problem occured.  Please contact administrator.");
+            console.log("Unknown problem occured.  Please contact administrator.  Code: Manage-Applications-004");
+            this.loading.dismissAll();
+          }
+        );
+      //===============================
+    }
   }
 
 }
