3bb11c82a04d028d6b0b0fee10d23ba607355ed4
Vishal Gandhi <vkgandhi84@gmail.com>
2018-07-27 08:42:55 -0400

    Made some progress on profile page.

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,7 @@ import { ActionSheetController } from 'ionic-angular'
 import { Http } from "@angular/http";
 import { ShareProvider } from "../../services/share";
 import { AlertController } from 'ionic-angular';
+import { LoadingController, Loading } from 'ionic-angular'
 
 @IonicPage()
 @Component({
@@ -12,6 +13,8 @@ import { AlertController } from 'ionic-angular';
   templateUrl: "profile.html"
 })
 export class ProfilePage {
+  loading: Loading;
+
   data: any = {};
   following = false;
   signingIn = false;
@@ -48,7 +51,8 @@ export class ProfilePage {
     public navCtrl: NavController,
     public toastCtrl: ToastService,
     public modalCtrl: ModalController,
-    public actionSheetCtrl: ActionSheetController
+    public actionSheetCtrl: ActionSheetController,
+    public loadingCtrl: LoadingController
   ) { }
 
   eventsPage() {
@@ -69,6 +73,7 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadProfileInfo();
     console.log("Hello ProfileFour Page");
   }
 
@@ -92,6 +97,95 @@ export class ProfilePage {
 
   like(post) {
     this.toastCtrl.create("Like clicked");
+  }
+
+  loadProfileInfo() {
+    this.loading = this.loadingCtrl.create({
+      content: '',
+    });
+    this.loading.present();
+    //-----
+    var decoded_response = "";
+    var body = new FormData();
+    body.append('sessionid', this.shareProvider.sessionid);
+    this.http
+      .post(this.shareProvider.server + "profile/profile.php", body)
+      .subscribe(
+        data => {
+          decoded_response = JSON.parse(data["_body"]);
+          //console.log(data["_body"]);
+          if (decoded_response[0] == "true") {
+            if(decoded_response[2]['is_member_approved'] == 0) {
+              this.navCtrl.push("ApplicationPage");
+              this.loading.dismissAll();
+            }
+            else {
+              if(decoded_response[2]['is_member_approved'] == 1) {
+                this.member.name = decoded_response[2]['first_name'] + ' ' + decoded_response[2]['last_name'];
+                this.member.chapter = decoded_response[2]['chapter_name'];
+              }
+              this.loading.dismissAll();
+            }
+          }
+          else {
+            if((decoded_response[1] == 'Session Expired.') || (decoded_response[1] == 'Invalid Session.')) {
+              this.navCtrl.push('LoginPage');
+              this.loading.dismissAll();
+            }
+            else {
+              this.data.error = "Unknown problem occured.  Please contact administrator.";
+              console.log("Unknown problem occured.  Please contact administrator. - L001");
+              this.loading.dismissAll();
+            }
+          }
+        },
+        error => {
+          this.data.error = "Unknown problem occured.  Please contact administrator.";
+          console.log("Unknown problem occured.  Please contact administrator. - L002");
+          this.loading.dismissAll();
+        }
+      );
+    //-----
+    this.loading = this.loadingCtrl.create({
+      content: '',
+    });
+    this.loading.present();
+    //-----
+    var decoded_response = "";
+    var body = new FormData();
+    body.append('sessionid', this.shareProvider.sessionid);
+    this.http
+      .post(this.shareProvider.server + "profile/profileinfo.php", body)
+      .subscribe(
+        data => {
+          decoded_response = JSON.parse(data["_body"]);
+          //console.log(data["_body"]);
+          if (decoded_response[0] == "true") {
+            this.member.status = decoded_response[2]['type_of_membership'];
+            this.member.title = decoded_response[2]['member_title'];
+            this.member.memberSince = new Date(decoded_response[2]['dttmcreated']).getFullYear();
+            this.member.following = decoded_response[2]['total_miles'];
+            this.loading.dismissAll();
+          }
+          else {
+            if((decoded_response[1] == 'Session Expired.') || (decoded_response[1] == 'Invalid Session.')) {
+              this.navCtrl.push('LoginPage');
+              this.loading.dismissAll();
+            }
+            else {
+              this.data.error = "Unknown problem occured.  Please contact administrator.";
+              console.log("Unknown problem occured.  Please contact administrator. - L001");
+              this.loading.dismissAll();
+            }
+          }
+        },
+        error => {
+          this.data.error = "Unknown problem occured.  Please contact administrator.";
+          console.log("Unknown problem occured.  Please contact administrator. - L002");
+          this.loading.dismissAll();
+        }
+      );
+    //-----
   }
 
 }
