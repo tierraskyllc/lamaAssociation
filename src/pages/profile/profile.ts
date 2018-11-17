7db2920ea64718d43cd4e052bdfc0f80e89ef155
Vishal Gandhi <vkgandhi84@gmail.com>
2018-11-17 03:15:28 -0500

    Made some progress on calendar.

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -55,7 +55,8 @@ export class ProfilePage {
   currentDate: any;
   mymonth = new Date().getMonth();
   myyear = new Date().getFullYear();
-  event = { title: "", location: "", message: "", startDate: "", endDate: "" };
+  //event = { title: "", location: "", message: "", start_dttm: "", end_dttm: "" };
+  event = {"id":0,"0":0,"title":"","1":"","description":"","2":"","start_dttm":"","3":"","end_dttm":"","4":"","type":"","5":""};
   eventList: any;
   selectedEvent: any;
   isSelected: any;
@@ -70,10 +71,12 @@ export class ProfilePage {
     public loadingCtrl: LoadingController
   ) {
     // Calendar //
-    this.date = new Date();
-    this.getDaysOfMonth();
     this.eventList = [];
-    this.eventList.push({ title: "First Test Event", location: "Location for First Test Event", message: "This is just a test event.", startDate: new Date('2018-11-17 15:20:00'), endDate: new Date('2018-11-18 22:00:00') })
+    this.date = new Date();
+    this.getCalendarMonth(this.date.getFullYear(), this.date.getMonth()+1);
+    this.getDaysOfMonth();
+    //this.getCalendarMonth(this.date.getFullYear(), this.date.getMonth());
+    //this.eventList.push({ title: "First Test Event", location: "Location for First Test Event", message: "This is just a test event.", start_dttm: new Date('2018-11-17 15:20:00'), end_dttm: new Date('2018-11-18 22:00:00') })
   }
 
   eventsPage() {
@@ -111,6 +114,7 @@ export class ProfilePage {
 
   ionViewDidLoad() {
     this.loadProfileInfo();
+    //this.getCalendarMonth(this.date.getFullYear(), this.date.getMonth()+1);
     console.log("Hello ProfileFour Page");
   }
 
@@ -299,14 +303,14 @@ export class ProfilePage {
     var thisDate1 = new Date(this.date.getFullYear()+"-"+(this.date.getMonth()+1)+"-"+day+" 00:00:00");
     var thisDate2 = new Date(this.date.getFullYear()+"-"+(this.date.getMonth()+1)+"-"+day+" 23:59:59");
     this.eventList.forEach(event => {
-      //var eventStartDate = new Date(event.startDate);
-      //var eventEndDate = new Date(event.endDate);
-      //console.log(event.startDate);
-      //console.log(event.endDate);
+      //var eventStartDate = new Date(event.start_dttm);
+      //var eventEndDate = new Date(event.end_dttm);
+      //console.log(event.start_dttm);
+      //console.log(event.end_dttm);
       //console.log(thisDate1);
       //console.log(thisDate2);
-      //if((event.startDate >= thisDate1) && (event.endDate >= thisDate1) && (event.endDate >= thisDate2)) {
-      if(((event.startDate >= thisDate1) && (event.endDate <= thisDate2)) || ((thisDate2 >= event.startDate) && (thisDate2 <= event.endDate)) || ((thisDate1 >= event.startDate) && (thisDate1 <= event.endDate))) {
+      //if((event.start_dttm >= thisDate1) && (event.end_dttm >= thisDate1) && (event.end_dttm >= thisDate2)) {
+      if(((event.start_dttm >= thisDate1) && (event.end_dttm <= thisDate2)) || ((thisDate2 >= event.start_dttm) && (thisDate2 <= event.end_dttm)) || ((thisDate1 >= event.start_dttm) && (thisDate1 <= event.end_dttm))) {
         hasEvent = true;
       }
       //console.log(hasEvent);
@@ -322,14 +326,73 @@ export class ProfilePage {
     //console.log('checkpoint-1');
     this.eventList.forEach(event => {
       //console.log('checkpoint-2');
-      //if(((event.startDate >= thisDate1) && (event.startDate <= thisDate2)) || ((event.endDate >= thisDate1) && (event.endDate <= thisDate2))) {
-      if(((event.startDate >= thisDate1) && (event.endDate <= thisDate2)) || ((thisDate2 >= event.startDate) && (thisDate2 <= event.endDate)) || ((thisDate1 >= event.startDate) && (thisDate1 <= event.endDate))) {
+      //if(((event.start_dttm >= thisDate1) && (event.start_dttm <= thisDate2)) || ((event.end_dttm >= thisDate1) && (event.end_dttm <= thisDate2))) {
+      if(((event.start_dttm >= thisDate1) && (event.end_dttm <= thisDate2)) || ((thisDate2 >= event.start_dttm) && (thisDate2 <= event.end_dttm)) || ((thisDate1 >= event.start_dttm) && (thisDate1 <= event.end_dttm))) {
         this.isSelected = true;
         this.selectedEvent.push(event);
         //console.log('checkpoint-3');
       }
       //console.log('checkpoint-4');
     });
+  }
+
+  getCalendarMonth(year, month) {
+    console.log(year);
+    console.log(month);
+    this.loading = this.loadingCtrl.create({
+      content: ""
+    });
+    this.loading.present();
+    //-----
+    var decoded_response = "";
+    var body = new FormData();
+    body.append("sessionid", this.shareProvider.sessionid);
+    body.append("year", year);
+    body.append("month", month);
+    this.http
+      .post(this.shareProvider.server + "profile/calendar.php", body)
+      .subscribe(
+        data => {
+          decoded_response = JSON.parse(data["_body"]);
+          //console.log(data["_body"]);
+          if (decoded_response[0] == "true") {
+            //console.log(decoded_response[2]);
+            for(var i=0; i<decoded_response[2].length; i++) {
+              decoded_response[2][i]['start_dttm'] = new Date(decoded_response[2][i]['start_dttm']);
+              decoded_response[2][i]['end_dttm'] = new Date(decoded_response[2][i]['end_dttm']);
+              this.eventList.push(decoded_response[2][i]);
+              //this.eventList.push({ title: "First Test Event", location: "Location for First Test Event", message: "This is just a test event.", start_dttm: new Date('2018-11-17 15:20:00'), end_dttm: new Date('2018-11-18 22:00:00') });
+              //console.log(decoded_response[2][i]["id"]);
+              //console.log(decoded_response[2][i]["title"]);
+            }
+            this.loading.dismissAll();
+          } else {
+            if (
+              decoded_response[1] == "Session Expired." ||
+              decoded_response[1] == "Invalid Session."
+            ) {
+              this.navCtrl.push("LoginPage");
+              this.loading.dismissAll();
+            } else {
+              this.data.error =
+                "Unknown problem occured.  Please contact administrator.";
+              console.log(
+                "Unknown problem occured.  Please contact administrator. - CAL001"
+              );
+              this.loading.dismissAll();
+            }
+          }
+        },
+        error => {
+          this.data.error =
+            "Unknown problem occured.  Please contact administrator.";
+          console.log(
+            "Unknown problem occured.  Please contact administrator. - CAL002"
+          );
+          this.loading.dismissAll();
+        }
+      );
+    //-----
   }
 
 }
