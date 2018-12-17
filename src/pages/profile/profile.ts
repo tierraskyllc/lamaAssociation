e74a035c173a9d4e107349ceab1358306c9ac2d6
Vishal Gandhi <vgandhi@fdu.edu>
2018-12-16 20:54:18 -0500

    Users can now change their profile pic.

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,10 +1,15 @@
 import { Component } from "@angular/core";
 import { NavController, IonicPage, ModalController } from "ionic-angular";
-import { ToastService } from "../../services/toast.service";
+//import { ToastService } from "../../services/toast.service";
 import { ActionSheetController } from "ionic-angular";
 import { Http } from "@angular/http";
 import { ShareProvider } from "../../services/share";
-import { LoadingController, Loading } from "ionic-angular";
+import { LoadingController, Loading, Platform, ToastController } from "ionic-angular";
+import { Camera } from '@ionic-native/camera';
+import { File } from '@ionic-native/file';
+import { FilePath } from '@ionic-native/file-path';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -13,19 +18,23 @@ import { LoadingController, Loading } from "ionic-angular";
 })
 export class ProfilePage {
   loading: Loading;
-
+  lastImage: string = "";
+  lastImageFullPath: string = "";
+  isUploadImageRunning: boolean = false;
   data: any = {};
   following = false;
   signingIn = false;
   underConstruction = false;
   member = {
     name: "Name from Server",
-    profileImage: "assets/images/avatar/girl-avatar.png",
+    //profileImage: "assets/images/avatar/girl-avatar.png",
+    profileImage: "assets/images/avatar/nopic.png",
     profileQrImage: "assets/images/qr-code.png",
     coverImage: "assets/images/background/background-5.jpg",
     chapter: "L.A.M.A. Newark",
     status: "Full Pathed Member",
     title: "Road Captain",
+    profile_pic: null,
     followers: 456,
     following: 1051,
     memberSince: 2003
@@ -65,10 +74,17 @@ export class ProfilePage {
     private http: Http,
     private shareProvider: ShareProvider,
     public navCtrl: NavController,
-    public toastCtrl: ToastService,
+    //public toastCtrl: ToastService,
     public modalCtrl: ModalController,
     public actionSheetCtrl: ActionSheetController,
-    public loadingCtrl: LoadingController
+    public loadingCtrl: LoadingController,
+    private camera: Camera,
+    public platform: Platform,
+    private filePath: FilePath,
+    private file: File,
+    public toastCtrl: ToastController,
+    private transfer: FileTransfer,
+    private alertCtrl: AlertController
   ) {
   }
 
@@ -110,34 +126,38 @@ export class ProfilePage {
     this.date = new Date();
     this.getCalendarMonth(this.date.getFullYear(), this.date.getMonth()+1);
     this.getDaysOfMonth();
+    setInterval(() => {      
+      //console.log('timer');
+      this.uploadImage();
+      },2000);
   }
 
   ionViewDidLoad() {
     this.loadProfileInfo();
     //this.getCalendarMonth(this.date.getFullYear(), this.date.getMonth()+1);
-    console.log("Hello ProfileFour Page");
+    //console.log("Hello ProfileFour Page");
   }
 
   signIn() {
     this.signingIn = !this.signingIn;
-    this.toastCtrl.create("Signing In member clicked");
+    //this.toastCtrl.create("Signing In member clicked");
   }
 
   follow() {
     this.following = !this.following;
-    this.toastCtrl.create("Follow member clicked");
+    //this.toastCtrl.create("Follow member clicked");
   }
 
   imageTapped(post) {
-    this.toastCtrl.create("Post image clicked");
+    //this.toastCtrl.create("Post image clicked");
   }
 
   comment(post) {
-    this.toastCtrl.create("Comments clicked");
+    //this.toastCtrl.create("Comments clicked");
   }
 
   like(post) {
-    this.toastCtrl.create("Like clicked");
+    //this.toastCtrl.create("Like clicked");
   }
 
   loadProfileInfo() {
@@ -217,6 +237,10 @@ export class ProfilePage {
               decoded_response[2]["dttmcreated"].replace(/-/g,'/')
             ).getFullYear();
             this.member.following = decoded_response[2]["total_miles"];
+            if(!((decoded_response[2]["profile_pic"] === 'null') || (decoded_response[2]["profile_pic"] == null))) {
+              this.member.profile_pic = decoded_response[2]["profile_pic"];
+              this.displayProfilePic(this.member.profile_pic);
+            }
             this.loading.dismissAll();
           } else {
             if (
@@ -415,5 +439,226 @@ export class ProfilePage {
     minutes = minutes < 10 ? '0'+minutes : minutes;
     var strTime = hours + ':' + minutes + ' ' + ampm;
     return strTime;
+  }
+
+  public takePicture(sourceType) {
+    // Create options for the Camera Dialog
+    var options = {
+      quality: 100,
+      sourceType: sourceType,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+   
+    // Get the data of an image
+    this.camera.getPicture(options).then((imagePath) => {
+      // Special handling for Android library
+      if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+        this.filePath.resolveNativePath(imagePath)
+          .then(filePath => {
+            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+            let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+            this.lastImageFullPath = correctPath + currentName;
+          });
+      } else {
+        //this.presentMessageOnlyAlert('checkpoint-1');
+        //this.presentMessageOnlyAlert(imagePath);
+        var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+        //this.presentMessageOnlyAlert('checkpoint-2');
+        var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+        //this.presentMessageOnlyAlert('checkpoint-3');
+        //this.presentMessageOnlyAlert(correctPath);
+        //this.presentMessageOnlyAlert(currentName);
+        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+        //this.presentMessageOnlyAlert('checkpoint-4');
+        this.lastImageFullPath = imagePath;
+      }
+      //this.uploadImage();
+    }, (err) => {
+      this.presentToast('Error while selecting image.' + err);
+    });
+  }
+
+  // Create a new name for the image
+  private createFileName() {
+    var d = new Date(),
+    n = d.getTime(),
+    newFileName =  this.shareProvider.username + '_profile_pic_' + n + ".jpg";
+    //this.presentMessageOnlyAlert(newFileName);
+    return newFileName;
+  }
+  
+  // Copy the image to a local folder
+  private copyFileToLocalDir(namePath, currentName, newFileName) {
+    //this.presentMessageOnlyAlert(this.file.dataDirectory);
+    this.file.copyFile(namePath, currentName, this.file.dataDirectory, newFileName).then(success => {
+      this.lastImage = newFileName;
+    }, error => {
+      this.lastImage = newFileName;
+      //this.presentToast('Error while storing file.');
+      console.log('Error while storing file.  This error can be safely ignored.');
+    });
+  }
+  
+  private presentToast(text) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 10000,
+      position: 'top'
+    });
+    toast.present();
+  }
+  
+  // Always get the accurate path to your apps folder
+  public pathForImage(img) {
+    if (img === null) {
+      return '';
+    } else {
+      return this.file.dataDirectory + img;
+    }
+  }
+
+  public uploadImage() {
+    //this.presentToast('Inside uploadImage');
+    if((this.lastImage != "") && (this.lastImageFullPath != "") && (this.isUploadImageRunning != true)) {
+      //this.presentToast('Inside uploadImage');
+      this.isUploadImageRunning = true;
+      //this.presentToast(this.lastImage);
+      //this.presentToast(this.lastImageFullPath);
+    // Destination URL
+      var url = this.shareProvider.server + "application/upload.php";
+    
+      // File for Upload
+      var targetPath = this.pathForImage(this.lastImage);
+    
+      // File name only
+      var filename = this.lastImage;
+    
+      var options = {
+        fileKey: "file",
+        fileName: filename,
+        chunkedMode: false,
+        mimeType: "multipart/form-data",
+        params : {'fileName': filename, 'sessionid': this.shareProvider.sessionid}
+      };
+    
+      const fileTransfer: FileTransferObject = this.transfer.create();
+    
+      this.loading = this.loadingCtrl.create({
+        content: 'Uploading...',
+      });
+      this.loading.present();
+    
+      // Use the FileTransfer to upload the image
+      fileTransfer.upload(this.lastImageFullPath, url, options).then((data) => {
+        this.updateProfilePicInDB(this.lastImage);
+        this.loading.dismissAll();
+        this.presentToast('Image succesful uploaded.');
+        //this.presentToast(this.lastImage);
+        this.lastImage = "";
+        this.lastImageFullPath = "";
+        this.isUploadImageRunning = false;
+      }, (err) => {
+        this.loading.dismissAll();
+        this.presentToast('Error while uploading image(s).');
+        this.isUploadImageRunning = false;
+      });
+    }
+  }
+
+  public changeProfilePic() {
+    this.takePicture(this.camera.PictureSourceType.CAMERA);
+  }
+
+  updateProfilePicInDB(lastImage) {
+    //this.submitAttempt = true;
+    //if (this.editProfileForm.valid) {
+      var body = new FormData();
+      var json_encoded_response = "";
+      var decoded_response = "";
+      body.append('sessionid', this.shareProvider.sessionid);
+      body.append("profile_pic", lastImage);
+      this.http
+        .post(this.shareProvider.server + "profile/changeprofilepic.php", body)
+        .subscribe(
+          data => {
+            json_encoded_response = data["_body"];
+            //console.log(json_encoded_response);
+            decoded_response = JSON.parse(json_encoded_response);
+            //console.log(decoded_response);
+            if (decoded_response[0] === "error") {
+              this.data.error = decoded_response[1];
+              this.presentMessageOnlyAlert(decoded_response[1]);
+            } else {
+              if (decoded_response[0]) {
+                console.log(decoded_response[2]);
+                this.displayProfilePic(lastImage);
+                //this.presentMessageOnlyAlert(decoded_response[2]);
+                //this.navCtrl.pop();
+              } 
+              else if (!decoded_response[0]) {
+                this.data.error = decoded_response[2];
+                this.presentMessageOnlyAlert(decoded_response[2]);
+              } else {
+                this.data.error = "Problem changing profile picture.  Please check your internet connection.  Contact administrator if problem persists.";
+                this.presentMessageOnlyAlert("Problem changing profile picture.  Please check your internet connection.  Contact administrator if problem persists.");
+              }
+            }
+          },
+          error => {
+            this.data.error =
+              "Problem changing profile picture.  Please check your internet connection.  Contact administrator if problem persists.";
+          }
+        );
+    //}
+  }
+
+  presentMessageOnlyAlert(alertmsg: string) {
+    let alert = this.alertCtrl.create({
+      message: alertmsg,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  displayProfilePic(profile_pic) {
+      /*this.loading = this.loadingCtrl.create({
+        content: '',
+      });
+      this.loading.present();*/
+
+      var body = new FormData();
+      body.append('sessionid', this.shareProvider.sessionid);
+      ////body.append('docname', motorcyclesobjects[num]['odometerPic']);
+      body.append('docname', profile_pic);
+      this.http.post(this.shareProvider.server + "docdownload/tempsession.php", body).subscribe(
+        data => {
+          var decoded_response = JSON.parse(data["_body"]);
+          //console.log(data["_body"]);
+          if (decoded_response[0] == "true") {
+            //console.log(decoded_response[1]);
+            //----------
+            var docurl = this.shareProvider.server + "docdownload/downloaddoc.php?temporaryshortsessionid=" + decoded_response[1] + "&docname=" + profile_pic;
+            this.member.profileImage = docurl;
+            //console.log(this.member.profileImage);
+            //----------
+            //this.loading.dismissAll();
+          }
+          else {
+            //this.data.error = "Unknown problem occured.  Please contact administrator.";
+            this.presentMessageOnlyAlert("Problem displaying profile picture.  Please contact administrator.");
+            console.log("Problem displaying profile picture.  Please contact administrator.");
+            //this.loading.dismissAll();
+          }
+        },
+        error => {
+          //this.data.error = "Unknown problem occured.  Please contact administrator.";
+          //console.log("Oooops!");
+          this.presentMessageOnlyAlert("Problem displaying profile picture.  Please contact administrator.");
+          console.log("Problem displaying profile picture.  Please contact administrator.");
+          //this.loading.dismissAll();
+        }
+      );
   }
 }
